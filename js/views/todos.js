71f07c76616f2f6b952da6ebcfc376a4df8527cd
Inumidun Amao <inumidunamao@Inumiduns-MacBook-Pro.local>
2017-11-17 09:45:51 +0100

    bare minimun backbone todo app

---
+++ js/views/todos.js
@@ -0,0 +1,79 @@
+var app = app || {};
+
+var TodoView = Backbone.View.extend({
+  tagName: 'li',
+
+  id: 'no-li-style',
+
+  template: _.template($('#item-template').html()),
+
+  events: {
+    'click .toggle': 'togglecompleted',
+    'click .destroy': 'clear',
+    'dblclick label': 'edit',
+    'keypress .edit': 'updateOnEnter',
+    'blur .edit': 'close'
+  },
+
+  initialize: function() {
+    this.listenTo(this.model, 'change', this.render);
+    this.listenTo(this.model, 'destroy', this.remove);
+    this.listenTo(this.model, 'visible', this.toggleVisible);
+  },
+
+  render: function() {
+    this.$el.html(this.template(this.model.toJSON()));
+
+    this.$el.toggleClass('completed', this.model.get('completed'));
+    this.toggleVisible();
+
+    this.$input = this.$('.edit');
+    return this;
+  },
+
+  toggleVisible: function() {
+    this.$el.toggleClass('hidden', true);
+  },
+
+  isHidden: function() {
+    var isCompleted = this.model.get('completed');
+    return (
+      (!isCompleted && app.TodoFilter === 'completed')
+      ||
+      (isCompleted && app.TodoFilter === 'active')
+    );
+  },
+
+  togglecompleted: function() {
+    this.model.toggle();
+  },
+
+  edit: function() {
+    this.$el.addClass('editing');
+    this.$input[0].disabled = false;
+    this.$input.focus();
+  },
+
+  close: function() {
+    this.$input[0].disabled = true;
+    var value = this.$input.val().trim();
+
+    if(value) {
+      this.model.save({ title: value });
+    }
+
+    this.$el.removeClass('editing');
+  },
+
+  updateOnEnter: function(e) {
+    if (e.which === ENTER_KEY) {
+      this.close();
+    }
+  },
+
+  clear: function () {
+    this.model.destroy();
+  }
+});
+
+app.TodoView = TodoView;
